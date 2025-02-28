b64557f6e20884f35f888a30e6e0394c6a5e6825
remon <remosamir443@gmail.com>
2025-02-28 03:21:23 -0800

    upload

--- src/Dashboard/UserDashboard/Main/Right/MainUserDashboardRight.js
+++ src/Dashboard/UserDashboard/Main/Right/MainUserDashboardRight.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./MainUserDashboardRight.css";
+import OrderMenu from "./OrderMenu/OrderMenu";
 const MainUserDashboardRight = () => {
   return (
     <div className="MainUserDashboardRight">
@@ -29,12 +30,34 @@ const MainUserDashboardRight = () => {
         <h3>your wallet</h3>
         <div className="ballance">
           <div className="text">
-          <p className="header">ballance</p>
-          <p className="price">$500 </p>
-
+            <p className="header">ballance</p>
+            <p className="price">$500 </p>
           </div>
         </div>
       </div>
+      <div className="address">
+        <h3 className="title">your address</h3>
+        <div className="data">
+          <div>
+            <img
+              src={require("../../../../images/Location-colored.png")}
+              alt="location"
+              loading="lazy"
+            />
+            <span>Elm Street, 23</span>
+          </div>
+          <button>Change</button>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt.{" "}
+        </p>
+        <div className="adds">
+          <button>add details</button>
+          <button>add notes</button>
+        </div>
+      </div>
+      <OrderMenu/>
     </div>
   );
 };
