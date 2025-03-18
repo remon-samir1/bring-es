9b62c22874acc125abe3391aa63442849704c2d3
remon <remosamir443@gmail.com>
2025-03-18 16:39:25 -0700

    first upload

--- src/Dashboard/AdminDahboard/AdminDahboard.js
+++ src/Dashboard/AdminDahboard/AdminDahboard.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './AdminDahboard.css'
 import TopBar from './TopBar/TopBar';
 import Invoices from './Invoices/Invoices';
+import BChart from './Charts/BarChart/BarChart';
 // import AdminSideBar from './SideBar/AdminSideBar';
 const AdminDahboard = () => {
   return (
     <div className='px-3'>
       <TopBar/>
       <Invoices/>
+      <div className='BarChart'>
+       <div className="header">
+        <h3>Revenue</h3>
+       </div>
+      <BChart/>
+      </div>
     </div>
   );
 }
--- src/Dashboard/AdminDahboard/Charts/BarChart/BarChart.js
+++ src/Dashboard/AdminDahboard/Charts/BarChart/BarChart.js
@@ -1,58 +1,87 @@
-
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
   {
-    name: 'Page A',
+    name: 'Jan',
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: 'Feb',
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: 'Mar',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: 'Apr',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: 'May',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: 'Jun',
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: 'Jul',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Aug',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Sep',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Oct',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Nov',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'Dec',
     uv: 3490,
     pv: 4300,
     amt: 2100,
   },
 ];
 
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/p/sandbox/stacked-bar-chart-7fwfgj';
+export default class BChart extends PureComponent {
+  // static demoUrl = 'https://codesandbox.io/p/sandbox/stacked-bar-chart-7fwfgj';
 
   render() {
     return (
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="100%" height="95%">
         <BarChart
           width={500}
           height={300}
@@ -68,9 +97,9 @@ export default class Example extends PureComponent {
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-          <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+          {/* <Legend /> */}
+          <Bar dataKey="pv" stackId="a" fill="#9E0529" />
+          <Bar dataKey="uv" stackId="a" fill="#F5E6EA" />
         </BarChart>
       </ResponsiveContainer>
     );
